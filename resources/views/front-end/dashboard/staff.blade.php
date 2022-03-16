391ced13971cb610370c426e0df65c20a21d9ef4
junaidnasir1001 <45687809+junaidnasir1001@users.noreply.github.com>
2022-03-16 15:44:35 +0500

    1st error resolve

--- resources/views/front-end/dashboard/staff.blade.php
+++ resources/views/front-end/dashboard/staff.blade.php
@@ -38,16 +38,6 @@
         </div>
         <div class="col-lg-3 col-md-4 col-6 text-center mb-3">
             <div class="ans-category-item pt-3">
-                <a href="{{route('jobs.create')}}" class="category-item p-0">
-                    <div class="category-icon mb-3">
-                    <i class="flaticon-job"></i>
-                    </div>
-                    <h6 class="ans-category-box-heading">Post Job</h6>
-                </a>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-4 col-6 text-center mb-3">
-            <div class="ans-category-item pt-3">
                 <a href="{{url('company/sites/create')}}" class="category-item p-0">
                     <div class="category-icon mb-3">
                     <i class="flaticon-worker"></i>
